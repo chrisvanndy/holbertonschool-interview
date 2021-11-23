9be020ac98e4e66faeeedec7827d321b3dd06027
Chris Vanndy <2736@holbertonschool.com>
2021-11-23 09:57:54 -0600

    pep8

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,36 +3,27 @@
 
 def minOperations(n):
     """Returns minimum number operations to yeild n
-    
-    Arguments: n, number of characters to be printed. cp, use of both the copy and paste function. ops, the total number of operations performed.
-    
+
+    Arguments:
+    n: chars to print
+    ops: operations to print chars
+    cp: copy paste actions (2)
+
     Returns:  the total number of opertaions required to yield n"""
-    
+
     ops = 0
     cp = 2
 
-    # as the loop runs, n will be divided down the accomodate chars left to print
+    # while looping, n divided down the accomodate chars left to print
     while n > 1:
-        # if characters left to print is a factor of 2 (copy + paste = 2 actions)
+        # if characters left to print is a factor of 2
         if (n % cp == 0):
             # add number of actions to operations variable
             ops += cp
             # half n to account for characters still needed to print
             n /= cp
         else:
-          # if n is not a factor of 2, paste 1 additional character and try again.
+          # if n is not a factor of 2, paste 1 try again.
             cp += 1
 
     return ops
-
-    
- 
-
-
-    
-        
-    return operations
-    
-
-
-
