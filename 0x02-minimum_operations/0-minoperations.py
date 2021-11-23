1cd17aae25738ea2e68e938b276fe64e2e06a545
Chris Vanndy <2736@holbertonschool.com>
2021-11-23 09:51:57 -0600

    pep8

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -13,14 +13,15 @@ def minOperations(n):
 
     # as the loop runs, n will be divided down the accomodate chars left to print
     while n > 1:
-      # if characters left to print is a factor of 2 (copy + paste = 2 actions)
-      if (n % cp == 0):
-        # add number of actions to operations variable
-        ops += cp
-        n /= cp
-      else:
-        # if n is not a factor of 2, paste 1 additional character and try again.
-        cp += 1
+        # if characters left to print is a factor of 2 (copy + paste = 2 actions)
+        if (n % cp == 0):
+            # add number of actions to operations variable
+            ops += cp
+            # half n to account for characters still needed to print
+            n /= cp
+        else:
+          # if n is not a factor of 2, paste 1 additional character and try again.
+            cp += 1
 
     return ops
 
