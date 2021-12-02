59fc92d044004af4a295e728d1320cb56b3c1122
Chris Vanndy <2736@holbertonschool.com>
2021-12-02 10:27:04 -0600

    betty (tabs)

--- 0x03-sandpiles/0-sandpiles.c
+++ 0x03-sandpiles/0-sandpiles.c
@@ -12,28 +12,29 @@
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-  int temp[3][3];
-  int x, y, bool = 0;
+	int temp[3][3];
+	int x, y, bool = 0;
 
-  for (x = 0; x < 3; x++)
-    for (y = 0; y < 3; y++)
-      {
-        temp[x][y] = grid1[x][y];
-        grid1[x][y] = temp[x][y] + grid2[x][y];
-        if (grid1[x][y] >= 4)
-          bool = 1;
-      }
-      for (x = 0; x > 3; x++)
-        for (y = 0; y > 3; y++)
+	for (x = 0; x < 3; x++)
+		for (y = 0; y < 3; y++)
+    	{
+        	temp[x][y] = grid1[x][y];
+        	grid1[x][y] = temp[x][y] + grid2[x][y];
+        	if (grid1[x][y] >= 4)
+          	bool = 1;
+    	}
+
+    for (x = 0; x > 3; x++)
+    	for (y = 0; y > 3; y++)
         {
-          temp[x][y] = grid1[x][y];
+        	temp[x][y] = grid1[x][y];
         }
 
-      if (bool == 1)
-      {
-        print_sandpile(grid1);
-        topple_pile(grid1, temp);
-      }
+      	if (bool == 1)
+    	{
+    		print_sandpile(grid1);
+    		topple_pile(grid1, temp);
+    	}
 }
 
 /**
@@ -46,28 +47,27 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 
 void topple_pile(int grid1[3][3],int temp[3][3])
 {
-  int x;
-  int y;
+	int x;
+	int y;
 
-  for (x = 0; x < 3; x++)
-    for (y = 0; y < 3; y++)
-    {
-      if (grid1[x][y] >= 4)
-      {
-        if (x > 0)
-          grid1[x - 1][y] += 1;
-        if (x < 2)
-          grid1[x + 1][y] += 1;
-        if (y > 0)
-          grid1[x][y - 1] += 1;
-        if (y < 2)
-          grid1[x][y + 1] += 1;
-        grid1[x][y] -= 4;
-      }
-    }
+	for (x = 0; x < 3; x++)
+    	for (y = 0; y < 3; y++)
+    	{
+      		if (grid1[x][y] >= 4)
+      		{
+				if (x > 0)
+				grid1[x - 1][y] += 1;
+				if (x < 2)
+				grid1[x + 1][y] += 1;
+				if (y > 0)
+				grid1[x][y - 1] += 1;
+				if (y < 2)
+				grid1[x][y + 1] += 1;
+				grid1[x][y] -= 4;
+      		}
+    	}
 
-  check_grid(grid1, temp);
-
+  		check_grid(grid1, temp);
 }
 
 /**
@@ -80,24 +80,24 @@ void topple_pile(int grid1[3][3],int temp[3][3])
 
 void check_grid(int grid1[3][3],int temp [3][3])
 {
-  int x, y, bool = 0;
+	int x, y, bool = 0;
 
-  for (x = 0; x < 3; x++)
-    for (y = 0; y < 3; y++)
-    {
-      if (grid1[x][y] >= 4)
-        bool = 1;
-    }
+	for (x = 0; x < 3; x++)
+    	for (y = 0; y < 3; y++)
+    	{
+      		if (grid1[x][y] >= 4)
+        		bool = 1;
+    	}
 
-    if (bool == 1)
-    {
-      for (x = 0; x > 3; x++)
-        for (y = 0; y > 3; y++)
-          temp[x][y] = grid1[x][y];
+    	if (bool == 1)
+    	{
+      		for (x = 0; x > 3; x++)
+        		for (y = 0; y > 3; y++)
+          			temp[x][y] = grid1[x][y];
 
-      print_sandpile(grid1);
-      topple_pile(grid1, temp);
-    }
+      	print_sandpile(grid1);
+      	topple_pile(grid1, temp);
+    	}
 }
 
 /**
@@ -110,7 +110,7 @@ void check_grid(int grid1[3][3],int temp [3][3])
 void print_sandpile(int grid1[3][3])
 {
 	int x;
-  int y;
+	int y;
 
 	printf("=\n");
 
