0d3f72bfb8a6adb7499bb99bf51ebcb8f38b09eb
Chris Vanndy <2736@holbertonschool.com>
2021-12-22 11:11:14 -0600

    pushing with print tests

--- 0x04-linked_list_palindrome/0-is_palindrome.c
+++ 0x04-linked_list_palindrome/0-is_palindrome.c
@@ -19,25 +19,26 @@ int is_palindrome(listint_t **head)
 
 
 /**
-* pali_helper - helper function determins if palindrome
+* pali_helper - helper function determines if linked list is palindrome
 * @left: left most node in list
-* @right: right most node in list
-* list is considered a palindrome if empty
+* @right: right most node in list (reverse comparision via recursion)
+* a list is considered a palindrome if empty
 * Return: 0 not a palindrome, 1 linked list is a palindrome
 */
 
 int pali_helper(listint_t **left, listint_t *right)
 {
 	/* empty list is a palindrome */
-	if (!right)
+	if (!right || !*left)
 		return (1);
 		
-	/* recursive method proves if linked list and its reverse */
+	/* recursive method returns 1 if linked list and its reverse */
 	/* are in fact a palindrome */
 	if (pali_helper(left, right->next) && (*left)->n == right->n)
 	{
 		/* traverse list */
 		*left = (*left)->next;
+		/* 	printf("%d\n ^^^ RIGHT\n", right->n); */
 		/* is a palindrome if all nodes meet if statement */
 		return (1);
 	}
